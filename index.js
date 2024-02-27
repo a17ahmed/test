4799b6f49655f4b5f3adee4a1dbc532ec5d880ed
Ahmed irfan <a17ahmed@gmail.com>
2024-02-27 15:29:18 +0500

    crud working

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const express = require('express');
-const multer = require('multer');
-const { S3Client, ListObjectsV2Command, GetObjectCommand, PutObjectCommand, DeleteObjectCommand } = require('@aws-sdk/client-s3');
-const { Upload } = require("@aws-sdk/lib-storage");
-const dotenv = require('dotenv');
-const cors= require('cors');
+import express from 'express';
+import multer from 'multer';
+import { 
+  S3Client, 
+  ListObjectsV2Command, 
+  GetObjectCommand, 
+  PutObjectCommand, 
+  DeleteObjectCommand 
+} from '@aws-sdk/client-s3';
+// import { Upload } from "@aws-sdk/lib-storage";
+import dotenv from 'dotenv';
+import cors from 'cors';
+import mime from 'mime';
+import fs from 'fs';
+
 
 dotenv.config();
 
@@ -14,7 +23,7 @@ const port = 3000;
 
 // Create S3 client with explicit credentials
 const s3Client = new S3Client({ 
-  region:"ap-south-1",
+  region:"eu-north-1",
     credentials:{
         accessKeyId:"AKIAT7VYXNPSFIOG6ZWV",
         secretAccessKey:"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",
@@ -44,26 +53,56 @@ const upload = multer({ dest: 'uploads/' });
   });
 
 // Upload a file to S3 bucket
+// app.post('/files', upload.single('file'), async (req, res) => {
+//    const file = req.file;
+//   if (!file) {
+//     res.status(400).json({ error: 'No file uploaded' });
+//     return;
+//   }
+//    const params = {
+//     Bucket: process.env.S3_BUCKET_NAME,
+//     Key: file.originalname,
+//     // Body:file.
+//   };
+ 
+//   try {
+//   const data=  await s3Client.send(new PutObjectCommand(params));
+//   console.log('data',data);
+//     res.json({ message: 'File uploaded successfully' });
+//   } catch (err) {
+//     console.error('Error uploading file:', err);
+//     res.status(500).json({ error: 'Internal server error' });
+//   }
+// });
+
 app.post('/files', upload.single('file'), async (req, res) => {
-   const file = req.file;
+  const file = req.file;
   if (!file) {
     res.status(400).json({ error: 'No file uploaded' });
     return;
   }
-   const params = {
+  const fileNameWithoutSpaces = file.originalname.replace(/\s+/g, '_');
+  const filePath = file.path;
+  const fileStream = fs.createReadStream(filePath);
+  const contentType = mime.getType(file.originalname) || 'application/octet-stream';
+
+  const params = {
     Bucket: process.env.S3_BUCKET_NAME,
-    Key: file.originalname,
-    // Body:file.
+    Key: fileNameWithoutSpaces,
+    Body: fileStream,
+    ContentType: contentType,
   };
- 
+
+  
   try {
-  const data=  await s3Client.send(new PutObjectCommand(params));
-  console.log('data',data);
+    const data = await s3Client.send(new PutObjectCommand(params));
+    console.log('data', data);
+    res.set('Content-Disposition', 'inline');
     res.json({ message: 'File uploaded successfully' });
   } catch (err) {
     console.error('Error uploading file:', err);
-    res.status(500).json({ error: 'Internal server error' });
-  }
+    res.status(500).json({ error: 'Internal server error' });
+ }
 });
 
 // Download a file from S3 bucket
@@ -73,6 +112,7 @@ app.get('/files/:fileName', async (req, res) => {
   try {
     const params = { Bucket: process.env.S3_BUCKET_NAME, Key: fileName };
     const data = await s3Client.send(new GetObjectCommand(params));
+    console.log("data**",data);
     res.attachment(fileName);
     res.send(data.Body);
   } catch (err) {
@@ -87,6 +127,7 @@ app.delete('/files/:fileName', async (req, res) => {
 
   try {
     const params = { Bucket: process.env.S3_BUCKET_NAME, Key: fileName };
+    console.log("key**",params.Key)
     await s3Client.send(new DeleteObjectCommand(params));
     res.json({ message: 'File deleted successfully' });
   } catch (err) {
